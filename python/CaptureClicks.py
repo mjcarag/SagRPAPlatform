3917acea7c54dd61f429773d5c5e92612bbfcf93
mjcarag <michaeljohn_1992@yahoo.com>
2025-03-14 10:19:12 -0400

    update keystrokes

--- python/CaptureClicks.py
+++ python/CaptureClicks.py
@@ -67,26 +67,81 @@ def Capture_Click(imagePath,app_title,clickType):
 #                 )
 #                 time.sleep(2)
 
+key_map = {
+    "ctrl": "ctrl",
+    "shift": "shift",
+    "alt": "alt",
+    "tab": "tab",
+    "caps": "capslock",
+    "esc": "esc",
+    "enter": "enter",
+    "space": "space",
+    "backspace": "backspace",
+    "delete": "delete",
+    "home": "home",
+    "end": "end",
+    "pageup": "pageup",
+    "pagedown": "pagedown",
+    "print": "printscreen",
+    "scroll": "scrolllock",
+    "pause": "pause",
+    "insert": "insert",
+    "arrowup": "up",
+    "arrowdown": "down",
+    "arrowleft": "left",
+    "arrowright": "right"
+}
+
+for i in range(1, 13):
+    key_map[f"f{i}"] = f"f{i}"
+
+def simulate_keystrokes(keys):
+    key_sequence = [key.strip() for key in keys.split('+')]
+
+    for key in key_sequence:
+        mapped_key = key_map.get(key.lower(), key)
+
+        if len(key) == 1 or key.isalpha() or key.isspace(): #for typing
+            print(f"Typing: {key}")
+            pyautogui.write(key)
+            # time.sleep(0.05)
+        elif len(key_sequence) > 1:# for combokeys like ctrl+c
+            
+            mapped_keys = [key_map.get(k.lower(), k) for k in key_sequence]
+            if all(mapped_keys):
+                print(f"Holding combo: {mapped_keys}")
+                for k in mapped_keys:
+                    pyautogui.keyDown(k)
+                time.sleep(0.1)
+                for k in mapped_keys:
+                    pyautogui.keyUp(k)
+                break
+        else:
+            # Handle special keys (like "Enter", "Backspace", etc.)
+            if mapped_key:
+                pyautogui.press(mapped_key)
+
 
 def main(jsondata):
     # with open(r'D:\Office Works\VSC\RPAUI\GithubBB\python\static\Testing\test.json') as json_file:
         # jdata = json.load(json_file)
         for item in jsondata['screenshots']:
             print (item['imagePath'])
-            Capture_Click(
-                item['imagePath'],
-                item['title'],
-                item['action']
-            )
+            if item['action'] == 'click':
+                Capture_Click(
+                    item['imagePath'],
+                    item['title'],
+                    item['action']
+                )
+            elif item['action'] == 'type':
+                simulate_keystrokes(item['keys'])
             time.sleep(2)
 
 if __name__ == "__main__":
-    # Accept JSON from command line argument
     if len(sys.argv) > 1:
         json_string = sys.argv[1]
         data = json.loads(json_string)
         time.sleep(5)
-        # main()
         main(data)
     else:
         print("No JSON data provided!")
