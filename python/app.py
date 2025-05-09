477424932d9946bc4edcbb69f19bd0c9fa7c4662
mjcarag <michaeljohn_1992@yahoo.com>
2025-05-09 14:10:49 +0000

    update cert and key

--- python/app.py
+++ python/app.py
@@ -729,7 +729,8 @@ def load_dataJson():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
-app.run(host="0.0.0.0", port=5000,  debug=True)
+# app.run(host="0.0.0.0", port=5000,  debug=True)
+app.run(host="0.0.0.0", port=5000, ssl_context=("cert.pem", "key.pem"))
 
 # port = int(os.environ.get("PORT", 5000))
 # app.run(host="0.0.0.0", port=port)
